2d683cb55edf087aa75c34c1a631f113f9570841
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-11-22 17:01:18 +0530

    creating task funtionality

--- server/server.js
+++ server/server.js
@@ -1,5 +1,37 @@
-import express from "express";
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const multer = require("multer");
+const cookieParser = require("cookie-parser");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(cookieParser());
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.listen(8800, () => {
+  console.log("API working!");
+});
