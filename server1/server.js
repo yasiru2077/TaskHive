c8b2b4f5e167cf8b43a0c0b3a7aaebf7bb5e9f89
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-11-30 05:25:59 +0530

    testing the new one

--- server1/server.js
+++ server1/server.js
@@ -3,3 +3,12 @@ const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`api working on port ${port}`);
+});
