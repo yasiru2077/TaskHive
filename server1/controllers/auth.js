bd5aa71773966bd5ea360ed1968159ffc6b2a9ee
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-12-02 20:41:39 +0530

    testing the new one

---
+++ server1/controllers/auth.js
@@ -0,0 +1,38 @@
+import { db } from "../connect.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const register = (req, res) => {
+  const q = "SELECT * FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    if (data.length) {
+      return res.status(409).json("User already exists");
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    const q =
+      "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)";
+
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      return res.status(200).json("User has been created");
+    });
+  });
+};
+
--- server1/server.js
+++ server1/server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
+const port = 5000;
+
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
@@ -18,7 +21,6 @@ app.use(
 
 app.use(cookieParser());
 
-const port = 5000;
 
 app.listen(port, () => {
   console.log(`api working on port ${port}`);
