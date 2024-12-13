3ac10c16d0c64faaba96c79eb959da4989e770db
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-12-13 10:21:27 +0530

    adding portfolio

---
+++ server2/constrollers/portfoilo.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) {
+    res.status(401).json("Not authenticated!");
+  }
+
+  jwt.verify(token, "secretKey", (err, userInfo) => {
+    if (err) {
+      return res.status(403).json("Token is not valid!");
+    }
+
+    req.userId = userInfo.id;
+    next();
+  });
+};
