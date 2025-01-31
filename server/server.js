1084bdae05d345c4f8097c234edabf062900c9e3
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-02-01 04:22:41 +0530

    adding the server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 const app = express();
 import authRoutes from "./routes/auth.js";
-import taksRoutes from "./routes/tasks.js";
+import tasksRoutes from "./routes/tasks.js";
 import cors from "cors";
 // import multer from "multer";
 import cookieParser from "cookie-parser";
+
+const port = 6000;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
@@ -13,14 +15,14 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:6000",
+    origin: `http://localhost:${port}`,
   })
 );
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taksRoutes);
+app.use("/api/tasks", tasksRoutes);
 
 app.listen(6000, () => {
-  console.log("API working!");
+  console.log(`API working on ${port}`);
 });
