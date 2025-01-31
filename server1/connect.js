8444f8073768c460f70ed7592e86bfb78846835c
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-02-01 03:27:56 +0530

    adding the server

--- server1/connect.js
+++
@@ -1,9 +0,0 @@
-import mysql2 from "mysql2";
-
-export const db = mysql2.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "faydro",
-  database: "taskhive",
-  port: 3308,
-});
