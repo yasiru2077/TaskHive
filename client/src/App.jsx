40e15302ff6773773f128d1b1db93d327c3b94da
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-02-02 20:13:33 +0530

    adding the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>fhaghfafafaf</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi minima
+        rem atque dolores impedit ullam excepturi in aspernatur laudantium
+        obcaecati numquam necessitatibus voluptatibus et culpa sequi, id
+        temporibus iste ab?
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
